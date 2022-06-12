79c22b23a37d97a83cf0f131a5e29743e3c5f95d
Tony Taylor <73037339+Crimsonsurf07@users.noreply.github.com>
2022-06-12 09:39:49 -0400

    seeds added

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+
+// Department Roles
+INSERT INTO departments (name) VALUES
+("Management"),
+("Development"),
+("Technical Support"),
+("Sales"),
+("Human Resources")
+
+// Roles Title, Salary, and ID
+INSERT INTO role ( title, salary, department_id) VALUES
+("CEO", 1000000, 1),
+("COO", 850000, 1),
+("VP of Operations", 700000, 1),
+("Regional Manager", 500000, 1),
+("Front End Developer", 90000, 2),
+("DB Admin", 110000, 2),
+("Help Desk 1", 55000, 3),
+("Help Desk 2", 61000, 3),
+("HR Rep", 50,000, 5);
